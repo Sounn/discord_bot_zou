4612aed766c35c888ee1f96a9bbfc035df304cb5
Sounn <kikuitisounn@gmail.com>
2020-07-06 15:19:44 +0900

    first commit/add/Zou

--- discordbot.rb
+++ discordbot.rb
@@ -5,10 +5,14 @@ require 'discordrb'
 TOKEN = ENV['DISCORD_BOT_TOKEN']
 TOKEN.freeze
 
-bot = Discordrb::Commands::CommandBot.new token: TOKEN, prefix: '/'
+bot = Discordrb::Commands::CommandBot.new token: TOKEN
 
-bot.command(:ping) do |event|
-  event.respond 'pong'
+# bot.command(:ping) do |event|
+#   event.respond 'pong'
+# end
+bot.message(content: 'Zou!') do |event|
+  # event.send_message("#{event.user.name}")
+  event.respond 'Paooooon!'
 end
 
 bot.run
